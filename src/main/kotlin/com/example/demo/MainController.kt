d2ea727f6facf92eb7e44f9985ccbe07ec3cecba
uijong0729 <krocha0729@gmail.com>
2021-10-02 19:19:22 +0900

    제길 인텔리제이 라이센스 끝남

--- src/main/kotlin/com/example/demo/MainController.kt
+++ src/main/kotlin/com/example/demo/MainController.kt
@@ -3,13 +3,24 @@ package com.example.demo
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("/")
 class MainController {
 
     @GetMapping("/main")
-    fun main():String{
+    fun main():String {
         return "enter"
+    }
+
+    @GetMapping("/sign_in")
+    fun signIn(
+        @RequestParam("email") email:String,
+        @RequestParam("password") password:String
+    ):String {
+        println(email)
+        println(password)
+        return "appMain"
     }
 }
